ddea0c7a9e0925dcae73ffebe5b3e63fc359f5ed
Sumapraja <irfansumapraja@gmail.com>
2019-07-01 12:05:54 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let now = new Date()
+console.log(now) // shows current date/time
+let selectedDate = new Date('2017-01-26')
+console.log(selectedDate)
+let date = new Date(2011, 0, 1, 2, 3, 4, 567)
+console.log(date) 
+// 1.01.2011, 02:03:04.567
+// current date
